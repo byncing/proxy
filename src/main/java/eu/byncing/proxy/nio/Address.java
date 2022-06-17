9b5555653f4e99cbec569f1df22aa0d6d77a9ba1
byncing <byncing@gmail.com>
2022-06-17 08:09:51 +0200

    Address structure update

--- src/main/java/eu/byncing/proxy/nio/Address.java
+++ src/main/java/eu/byncing/proxy/nio/Address.java
@@ -6,30 +6,30 @@ import java.nio.channels.SocketChannel;
 
 public interface Address {
 
-    static Address build(SocketChannel channel) {
+    static Address build(SocketAddress localAddress, SocketAddress remoteAddress) {
         return new Address() {
 
-            final SocketAddress localAddress, remoteAddress;
+            final SocketAddress localAddress0, remoteAddress0;
 
             {
-                try {
-                    localAddress = channel.getLocalAddress();
-                    remoteAddress = channel.getRemoteAddress();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+                localAddress0 = localAddress;
+                remoteAddress0 = remoteAddress;
             }
 
             @Override
             public SocketAddress localAddress() {
-                return localAddress;
+                return localAddress0;
             }
 
             @Override
             public SocketAddress remoteAddress() {
-                return remoteAddress;
+                return remoteAddress0;
             }
         };
+    }
+
+    static Address build(SocketChannel channel) throws IOException {
+        return build(channel.getLocalAddress(), channel.getRemoteAddress());
     }
 
     SocketAddress localAddress();
