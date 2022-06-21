36863eede34dc3eb5f66b10796b2bcab3b026173
byncing <byncing@gmail.com>
2022-06-21 02:43:47 +0200

    packet reading extension

--- src/main/java/eu/byncing/proxy/ProxyServer.java
+++ src/main/java/eu/byncing/proxy/ProxyServer.java
@@ -1,9 +1,16 @@
 package eu.byncing.proxy;
 
+import eu.byncing.proxy.connection.Connection;
+import eu.byncing.proxy.connection.PingedConnection;
 import eu.byncing.proxy.nio.KeyAction;
 import eu.byncing.proxy.nio.buf.NioBuf;
 import eu.byncing.proxy.nio.channel.Channel;
 import eu.byncing.proxy.nio.channel.NioChannel;
+import eu.byncing.proxy.protocol.handshake.PacketHandshake;
+import eu.byncing.proxy.protocol.login.PacketLoginStart;
+import eu.byncing.proxy.protocol.status.PacketLatency;
+import eu.byncing.proxy.protocol.status.PacketRequest;
+import eu.byncing.proxy.protocol.status.PacketResponse;
 
 import java.io.Closeable;
 import java.io.IOException;
@@ -13,13 +20,14 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ProxyServer implements Closeable {
 
+
     public static final ByteBuffer BUFFER = ByteBuffer.allocate(128);
+    public static final PingList PING_LIST = new PingList(new PingList.Version("Unstable", 0x00), new PingList.Players(22, 0), "Something different but nothing special");
 
     private boolean connected;
 
@@ -27,6 +35,7 @@ public class ProxyServer implements Closeable {
     private final ServerSocketChannel socket;
 
     private final Map<SocketAddress, Channel> channels = new HashMap<>();
+    private final Map<Channel, Connection> connections = new HashMap<>();
 
     public ProxyServer() throws IOException {
         this.action = new KeyAction();
@@ -36,7 +45,7 @@ public class ProxyServer implements Closeable {
     public static void main(String[] args) {
         try {
             ProxyServer server = new ProxyServer();
-            server.bind(new InetSocketAddress(25565), 1);
+            server.bind(new InetSocketAddress(25565), 128);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -56,10 +65,10 @@ public class ProxyServer implements Closeable {
                     socket.configureBlocking(false).register(action.selector(), SelectionKey.OP_READ);
 
                     Channel channel = new NioChannel(socket);
+                    Connection connection = new PingedConnection(channel);
 
                     channels.put(socket.getRemoteAddress(), channel);
-
-                    System.out.println(channel.remoteAddress() + " has connected.");
+                    connections.put(channel, connection);
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
@@ -74,7 +83,9 @@ public class ProxyServer implements Closeable {
                         channels.remove(channel.remoteAddress(), channel);
                         channel.future().close().execute();
 
-                        System.out.println(channel.remoteAddress() + " has disconnected.");
+                        Connection connection = connections.get(channel);
+                        connections.remove(channel, connection);
+
                         return;
                     }
 
@@ -85,7 +96,58 @@ public class ProxyServer implements Closeable {
                         byte[] bytes = new byte[length];
                         buf.readBytes(bytes, 0, length);
 
-                        System.out.println("data array: " + Arrays.toString(bytes));
+                        NioBuf packet = new NioBuf(bytes);
+                        int id = packet.readInt();
+
+                        PingedConnection connection = (PingedConnection) connections.get(channel);
+                        int protocol = connection.protocol();
+
+                        if (connection.state() == Connection.State.HANDSHAKE) {
+                            if (id == 0x00) {
+                                PacketHandshake handshake = new PacketHandshake();
+                                handshake.read(connection, packet);
+
+                                connection.protocol(handshake.protocol());
+
+                                if (handshake.state() == 1) {
+                                    connection.state(Connection.State.STATUS);
+                                } else connection.state(Connection.State.LOGIN);
+                            }
+                            continue;
+                        }
+
+                        if (connection.state() == Connection.State.STATUS) {
+                            if (id == 0x00) {
+                                PacketRequest request = new PacketRequest();
+
+                                NioBuf response = new NioBuf(0, false);
+                                PacketResponse packetResponse = new PacketResponse(PING_LIST);
+
+                                packetResponse.write(connection, response);
+                                channel.write(response).execute();
+                            }
+                            if (id == 0x01) {
+                                PacketLatency latency = new PacketLatency();
+                                latency.read(connection, packet);
+
+                                NioBuf response = new NioBuf(0, false);
+                                response.writeInt(0x01);
+                                response.writeLong(latency.payload());
+
+                                channel.write(response).execute();
+
+                                System.out.println(channel.remoteAddress() + " has pinged.");
+                            }
+                        }
+
+                        if (connection.state() == Connection.State.LOGIN) {
+                            if (id == 0x00) {
+                                PacketLoginStart login = new PacketLoginStart();
+                                login.read(connection, packet);
+
+                                System.out.println(login.name() + " has connected.");
+                            }
+                        }
                     }
                     buf.flush();
                 } catch (IOException e) {
