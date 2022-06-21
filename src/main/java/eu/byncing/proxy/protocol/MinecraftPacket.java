f17109f7dcdbec9ad268eb767b9dc3e2864a7705
byncing <byncing@gmail.com>
2022-06-21 02:38:16 +0200

    Added MinecraftPacket

--- src/main/java/eu/byncing/proxy/protocol/MinecraftPacket.java
+++ src/main/java/eu/byncing/proxy/protocol/MinecraftPacket.java
@@ -1,4 +1,13 @@
 package eu.byncing.proxy.protocol;
 
+import eu.byncing.proxy.connection.Connection;
+import eu.byncing.proxy.nio.buf.NioBuf;
+
 public class MinecraftPacket {
+
+    public void write(Connection connection, NioBuf buf) throws Exception {
+    }
+
+    public void read(Connection connection, NioBuf buf) throws Exception {
+    }
 }
